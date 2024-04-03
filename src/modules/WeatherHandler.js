4de28f4adc31a5dddb5f02d6a39498acab14bcc6
cariotic <j.symczyk@gmail.com>
2024-04-03 17:19:43 +0200

    Update url

--- src/modules/WeatherHandler.js
+++ src/modules/WeatherHandler.js
@@ -2,7 +2,7 @@
 export default class WeatherHandler {
 
     static async getCurrentWeather(location){
-        const url = `http://api.weatherapi.com/v1/current.json?key=4f101a1e978a444bad990331242803&q=${location}&aqi=no`;
+        const url = `https://api.weatherapi.com/v1/current.json?key=4f101a1e978a444bad990331242803&q=${location}&aqi=no`;
 
         try {
             const response = await fetch(url, {mode: 'cors'});
@@ -21,7 +21,7 @@ export default class WeatherHandler {
     }
 
     static async getWeatherForecast(location) {
-        const url = `http://api.weatherapi.com/v1/forecast.json?key=4f101a1e978a444bad990331242803&q=${location}&days=3&aqi=no&alerts=no`;
+        const url = `https://api.weatherapi.com/v1/forecast.json?key=4f101a1e978a444bad990331242803&q=${location}&days=3&aqi=no&alerts=no`;
 
         try {
             const response = await fetch(url, {mode: 'cors'});
